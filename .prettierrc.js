64f4601d2b57ea1b8d7daa89015989b0ed68504a
Jose Herradez <joseherradez@outlook.com>
2023-10-31 20:28:40 -0400

    Add ESlint + Prettier + Husky and TailwindCSS

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+export default {
+  arrowParens: "avoid",
+  bracketSameLine: true,
+  bracketSpacing: true,
+  singleQuote: true,
+  trailingComma: "all",
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  printWidth: 120,
+};
